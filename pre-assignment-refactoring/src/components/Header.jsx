8bf3f72cab28984ed3439bf03f54e8b4cc3a1f6a
chaengs <cathyshim222@naver.com>
2022-07-06 15:52:00 +0900

    #24 💄 헤더의 styled-component 분리

--- pre-assignment-refactoring/src/components/Header.jsx
+++ pre-assignment-refactoring/src/components/Header.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 import { MdHomeFilled } from "react-icons/md";
 import { FiSend } from "react-icons/fi";
@@ -8,6 +6,7 @@ import { GrLogout } from "react-icons/gr";
 import { FiPlusSquare } from "react-icons/fi";
 import { ImCompass2 } from "react-icons/im";
 import { AiOutlineHeart } from "react-icons/ai";
+import {StyledLink, Body, Bundle, Instagram, Input, Icons, Search} from './styles/headerStyle'
 
 export default function Header() {
   const storageClean = () => {
@@ -50,66 +49,3 @@ export default function Header() {
     </Body>
   );
 }
-
-const StyledLink = styled(Link)`
-  display: flex;
-  align-items: center;
-  color: black;
-`;
-
-const Body = styled.div`
-  display: flex;
-  align-items: center;
-  position: sticky;
-  top: 0;
-  height: 10vh;
-  min-width: 520px;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.25);
-`;
-
-const Bundle = styled.div`
-  @media (max-width: 675px) {
-    width: 80%;
-    margin: 0 1.5rem;
-  }
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 90%;
-  margin: 0 5rem;
-`;
-
-const Instagram = styled.div`
-  @media (max-width: 675px) {
-    font-size: 25px;
-  }
-  font-size: 35px;
-  font-weight: bold;
-`;
-
-const Input = styled.input`
-  padding: 10px 0 10px 10px;
-  background-color: rgba(0, 0, 0, 0);
-  border: none;
-  width: 20rem;
-`;
-
-const Icons = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  width: 15%;
-`;
-
-const Search = styled.div`
-  display: flex;
-  @media (max-width: 675px) {
-    display: none;
-    /* justify-content: space-between; */
-  }
-  align-items: center;
-  padding: 1px 10px;
-  width: 20vw;
-  background-color: rgba(0, 0, 0, 0.1);
-  border-radius: 8px;
-`;
